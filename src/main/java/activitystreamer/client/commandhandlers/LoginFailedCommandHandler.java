c620eeafa03dbe2d689573e680e8abe2a1fa0b68
Malcolm Karutz <malcolm_karutz@live.com>
2016-05-18 14:41:26 +1000

    fixed login failed bug

--- src/main/java/activitystreamer/client/commandhandlers/LoginFailedCommandHandler.java
+++ src/main/java/activitystreamer/client/commandhandlers/LoginFailedCommandHandler.java
@@ -1,13 +1,14 @@
 package activitystreamer.client.commandhandlers;
 
-import activitystreamer.core.command.*;
-import activitystreamer.core.commandhandler.*;
+import activitystreamer.core.command.Command;
+import activitystreamer.core.command.LoginFailedCommand;
+import activitystreamer.core.commandhandler.ICommandHandler;
 import activitystreamer.core.shared.Connection;
 
 public class LoginFailedCommandHandler implements ICommandHandler {
     @Override
     public boolean handleCommand(Command command, Connection conn) {
-        if (command instanceof LoginSuccessCommand) {
+        if (command instanceof LoginFailedCommand) {
             conn.close();
             return true;
         }
