7751e05ecee53260c2235470cbb206e9f7f8975f
Alex Zable <azable@student.unimelb.edu.au>
2016-05-29 21:36:44 +1000

    Submission

--- scripts/testBack.sh
+++ scripts/testBack.sh
@@ -4,3 +4,9 @@ sleep 2
 gnome-terminal -t server1 -e "java -jar build/libs/Server.jar -lp 4010 -slp 4011 -rh localhost -rp 4001 -d"
 sleep 2
 gnome-terminal -t server2 -e "java -jar build/libs/Server.jar -lp 4020 -slp 4021 -rh localhost -rp 4001 -d"
+sleep 2
+gnome-terminal -t server2 -e "java -jar build/libs/LegacyServer.jar -lp 4015 -rh localhost -rp 4010 -d"
+sleep 2
+gnome-terminal -t server2 -e "java -jar build/libs/LegacyServer.jar -lp 4025 -rh localhost -rp 4015 -d"
+sleep 2
+gnome-terminal -t server2 -e "java -jar build/libs/LegacyServer.jar -lp 4035 -rh localhost -rp 4025 -d"
